512a3b34ef181322763dabf5d5028d42837c33fe
camila-frangp <camila.frangp@gmail.com>
2017-07-11 18:52:12 -0400

    SE dan margenes a los iconos de los proyectos y se agrega a su función el likn de cada uno con jquery

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,7 @@
 $(document).ready( function(){
  // aqui va el codigo
  // alert("working");
- 	$.get('https://api.dribbble.com/v1/users/eduardo/shots?access_token=c86586c1235b2e5e9aef166131195f67e2f792cbe5cf7eaee7ea66923fc1a8ce', function(x) {
+ 	$.get('https://api.dribbble.com/v1/users/jonesandcompany/shots?access_token=c86586c1235b2e5e9aef166131195f67e2f792cbe5cf7eaee7ea66923fc1a8ce', function(x) {
 		//console.log(x);
 		// array 721
 		x.forEach(function(element){
@@ -10,20 +10,22 @@ $(document).ready( function(){
 			//console.log(element.pokemon_species.name[0]);
 			console.log(x);
 			console.log(element.images.normal);
-			$("#dribbble").append(	"<li><img src='" + element.images.normal + "'><div>" +
-										"<span class='view-icon'>" + 
-											"<i class='fa fa-eye' aria-hidden='true'></i>" + 
-											element.views_count + 
-										"</span>" + 
-										"<span class='coment-icon'>" +
-											"<i class='fa fa-comment' aria-hidden='true'></i>" + 
-										 	element.comments_count + 
-										 "</span>" + 
-										 "<span class='like-icon'>" +
-										 	"<i class='fa fa-heart' aria-hidden='true'></i>" +
-										 	element.likes_count + 
-										 "</span></div></li>");
+			$("#dribbble").append(	"<li class='port'><a href='" + element.html_url + "'>" + 
+										"<img class='img-responsive' src='" + element.images.normal + "'><div class='icon-port'>" +
+											"<span class='view-icon'>" + 
+												"<i class='fa fa-eye' aria-hidden='true'></i>" + " " +
+												element.views_count + 
+											"</span>" + 
+											"<span class='coment-icon'>" +
+												"<i class='fa fa-comment' aria-hidden='true'></i>" + " " +
+											 	element.comments_count + 
+											 "</span>" + 
+											 "<span class='like-icon'>" +
+											 	"<i class='fa fa-heart' aria-hidden='true'></i>" + " " +
+											 	element.likes_count + 
+											 "</span></div></li>");
 			
 		});
+
  	});
 });
