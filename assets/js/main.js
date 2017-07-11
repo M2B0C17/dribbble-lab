93b612c86df3a0772ec45ae6ca3aec518a660e11
pecorina <constanza.pecori@gmail.com>
2017-07-11 13:55:10 -0400

    hago mi rama avanzo en el nav

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,3 +3,15 @@ $(document).ready( function(){
  // aqui va el codigo
  // alert("working");
 });
+ 
+/*menu desplegable (cony)*/
+$(document).ready(function(){
+	$("#open-hide").click(function(){
+		$(this).toggleClass("show");
+	});
+});
+$(document).ready(function(){
+	$("#shotsi").click(function(){
+		$(this).toggleClass("show");
+	});
+});
