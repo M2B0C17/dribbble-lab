5d653758c1cabd36668d06edea765aaa9e33a2b7
RangiBecerra <rangibecerra@gmail.com>
2017-07-11 11:45:56 -0400

    lectura de imagenes por json

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,4 +2,15 @@
 $(document).ready( function(){
  // aqui va el codigo
  // alert("working");
+ $.get('https://api.dribbble.com/v1/users/eduardo/shots?access_token=c86586c1235b2e5e9aef166131195f67e2f792cbe5cf7eaee7ea66923fc1a8ce', function(x) {
+		//console.log(x);
+		// array 721
+		x.forEach(function(element){
+			// elemento
+			//console.log(element.pokemon_species.name[0]);
+			console.log(x);
+			console.log(element.images.normal);
+			$("#dribbble").append("<li><img src='" + element.images.normal + "'></li>")
+		});
+ });
 });
