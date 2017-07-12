7314cdb27c00d67522e6a2f1c8957ffab614ba94
kathmarioly <katerinemarioly@gmail.com>
2017-07-11 23:45:21 -0400

    Arreglos en función Jquery y css

--- assets/js/main.js
+++ assets/js/main.js
@@ -55,37 +55,4 @@ $(document).ready( function(){
  });
 
 /*hover kath*/
-$(document).ready( function(){
- // aqui va el codigo
- // alert("working");
- $.get('https://api.dribbble.com/v1/users/jonesandcompany/shots?access_token=c86586c1235b2e5e9aef166131195f67e2f792cbe5cf7eaee7ea66923fc1a8ce', function(x) {
-	     //console.log(x);
-        // array 721
-        x.forEach(function(element){
-            // elemento
-            //console.log(element.pokemon_species.name[0]);
-            console.log(x);
-            console.log(element.images.normal);
-            var  titulo = element.description;
-            $("#dribbble").append("<li class='port'><img class='img-responsive' src='" + element.images.normal + "'><div class='overlay'><div class='text'>" + element.title[0] + "</div></div>" +"<span class='view-icon'>" + 
-            	"<i class='fa fa-eye' aria-hidden='true'></i>" + 
-            	element.views_count + 
-            	"</span>" + 
-            	"<span class='coment-icon'>" +
-            	"<i class='fa fa-comment' aria-hidden='true'></i>" + 
-            	element.comments_count + 
-            	"</span>" + 
-            	"<span class='like-icon'>" +
-            	"<i class='fa fa-heart' aria-hidden='true'></i>" +
-            	element.likes_count + 
-            	"</span></div></li>");
-            $(".port").hover(function(){
-                //s$(".port").append("<div class='overlay'><div class='text'>" +element.title+ titulo + "</div></div>");
-                //$(".port").append("<div class='overlay'><div class='text'>" + element.title[0] + "</div></div>")
-                
-            })
-            
-        });
-    });
-});
 
